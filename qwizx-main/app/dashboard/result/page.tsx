1dd27f2ac594a629139a6b76bf68745f641918eb
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-07-10 00:41:58 +0530

    minor

--- qwizx-main/app/dashboard/result/page.tsx
+++ qwizx-main/app/dashboard/result/page.tsx
@@ -5,6 +5,7 @@ import { useParams } from 'next/navigation';
 import { useSearchParams } from 'next/navigation';
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
+import { Suspense } from 'react'
 
 const QuizResultPage = () => {
     const router = useRouter();
@@ -18,23 +19,25 @@ const QuizResultPage = () => {
 
     return (
         <div className="container mx-auto">
-            <Card className='h-[400px] shadow-md'>
-                <CardHeader>
-                    <CardTitle className="text-4xl font-bold mb-2 text-primary">Quiz Results</CardTitle>
-                    <CardDescription className="text-lg mb-6">Here&apos;s how you did!</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <div className="text-center">
-                        <p className="text-2xl mb-4 ">Your Score: {score} / {total}</p>
-                        <p className="text-xl mb-6">
-                            {Number(score) >= Number(total) / 2 ?
-                                <span className='text-primary'>Great job! You passed!</span> : <span className=''>Don&apos;t worry, try again sometime later!</span>
-                            }
-                        </p>
-                        <Button onClick={handleGoBack}>Go back to &apos;Take a Quiz&apos;</Button>
-                    </div>
-                </CardContent>
-            </Card>
+            <Suspense>
+                <Card className='h-[400px] shadow-md'>
+                    <CardHeader>
+                        <CardTitle className="text-4xl font-bold mb-2 text-primary">Quiz Results</CardTitle>
+                        <CardDescription className="text-lg mb-6">Here&apos;s how you did!</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="text-center">
+                            <p className="text-2xl mb-4 ">Your Score: {score} / {total}</p>
+                            <p className="text-xl mb-6">
+                                {Number(score) >= Number(total) / 2 ?
+                                    <span className='text-primary'>Great job! You passed!</span> : <span className=''>Don&apos;t worry, try again sometime later!</span>
+                                }
+                            </p>
+                            <Button onClick={handleGoBack}>Go back to &apos;Take a Quiz&apos;</Button>
+                        </div>
+                    </CardContent>
+                </Card>
+            </ Suspense>
         </div>
     );
 };
