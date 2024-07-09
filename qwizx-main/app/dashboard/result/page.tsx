ce5a5bab20f57e4d8e73e60149c07d722199428f
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-07-10 00:54:56 +0530

    minor

--- qwizx-main/app/dashboard/result/page.tsx
+++ qwizx-main/app/dashboard/result/page.tsx
@@ -19,8 +19,8 @@ const QuizResultPage = () => {
 
     return (
         <Suspense>
-            <div className="container mx-auto">
-
+        <div className="container mx-auto">
+            
                 <Card className='h-[400px] shadow-md'>
                     <CardHeader>
                         <CardTitle className="text-4xl font-bold mb-2 text-primary">Quiz Results</CardTitle>
@@ -38,8 +38,8 @@ const QuizResultPage = () => {
                         </div>
                     </CardContent>
                 </Card>
-
-            </div>
+            
+        </div>
         </ Suspense>
     );
 };
